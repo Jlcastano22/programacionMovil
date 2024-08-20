2fd1d7de566ae9d524f3c4b4c42a463c76308eaa
Jose Luis Castaño <jlcastanoa@outlook.es>
2024-08-19 19:02:26 -0500

    Avance entregable 1

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <view style={styles.estiloA}>
+        <text>A</text>
+      </view>
+      <view style={styles.estiloB}>
+        <text>B</text>
+      </view>
+      <view>
+        <view style={styles.estiloC}>
+          <text>C</text>
+        </view>
+        <view>
+          <text>D</text>
+        </view>
+        <view>
+          <text>E</text>
+        </view>
+      </view>
+      <view style={styles.estiloF}>
+        <text>F</text>
+      </view>
     </View>
   );
 }
@@ -15,6 +32,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    padding: 20,
+  },
+  estiloA: {
+    backgroundColor: '#aaa',
+    width: '90%',
+    height: 100,
+  },
+  estiloB: {
+    backgroundColor: '#aaa',
+    width: '90%',
+    height: 300,
+    marginTop: 20,
+  },
+  estiloC: {
+    backgroundColor: '#aaa',
+    width: '90%',
+    height: 100,
+  },
+  estiloF: {
+    backgroundColor: '#aaa',
+    width: '90%',
+    height: 100,
   },
 });
