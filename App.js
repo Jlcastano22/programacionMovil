014c70517396c2ef727a54828db8b4b68f7d8a8a
Jose Luis Castaño <jlcastanoa@outlook.es>
2024-08-30 18:10:25 -0500

    Entregable 2 funcional

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [suma, setSuma] = useState(0);
+  const [data, setData] = useState([]);
+  const urlBase = 'https://jsonplaceholder.typicode.com/';
+
+  const getBooks = () => {
+    fetch(urlBase + '/todos')
+      .then((response) => response.json())
+      .then((dataApi) => setData(dataApi))
+      .catch((error) => console.log(error));
+  };
+
+  const sumarCuenta = () => {
+    setSuma(suma + 1);
+  };
+
+  const restarCuenta = () => {
+    setSuma(suma - 1);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.estiloA}>
-        <Text>A</Text>
+        <Text>{suma}</Text>
+        <Button onPress={sumarCuenta} title='Contador + 1'></Button>
+        <Button onPress={restarCuenta} title='Contador - 1'></Button>
       </View>
       <View style={styles.estiloB}>
-        <Text>B</Text>
+        <Button onPress={getBooks} title='Llamar API'></Button>
+        <ScrollView>
+          <View style={styles.containerApi}>
+            {data.map((item) => (
+              <View style={styles.containerBooks}>
+                <Text>{item.id}</Text>
+                <Text>{item.title}</Text>
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+
+        {/* <SafeAreaView>
+          <FlatList
+            data={data}
+            keyExtactor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.containerBooks}>
+                <Text>{item.id}</Text>
+                <Text>{item.title}</Text>
+              </View>
+            )}
+          />
+        </SafeAreaView> */}
       </View>
       <View style={styles.estiloX}>
         <View style={styles.estiloC}>
@@ -28,6 +72,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  containerBooks: {
+    width: '35%',
+    height: 125,
+    backgroundColor: 'coral',
+    margin: 10,
+    alignItems: 'center',
+    padding: 10,
+  },
+  containerApi: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -43,10 +101,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   estiloB: {
+    padding: 10,
     backgroundColor: '#aaa',
     width: '90%',
     height: '40%',
-    marginTop: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
